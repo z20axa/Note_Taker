e22d91c7edb47480d162e8dd1fda5430d4a058a3
z20axa <alejandrospan@hotmail.com>
2022-12-23 15:44:00 -0500

    Adds post route for making a new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const data = require('../db/db.json');
 const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 
 router.get('/notes', (req, res) => {
@@ -8,17 +10,24 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
+
     // console.log(req);
     console.log(req.body);
     // if (typeof req.body.tittle !== 'undefined' &&
     //     typeof req.body.text !== 'undefined'
     // ) {
-        data.push({ ...req.body });
-        console.log(data);
-        // return fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(data), (err) => {
-        //     err ? console.error(err) : consgit ole.log("File Written!");
-        //     return res.status(200).json(req.body);
-    //     // });
+        data.push({ ...req.body, id:uuidv4() });
+        // console.log(data);
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(data), (err) => {
+            // err ? console.error(err) : console.log("File Written!");
+            if (err) {
+                console.error(err);
+                res.status(500).send("Error!");
+            } else {
+                res.status(200).json(req.body);
+            }
+                
+        });
     // }
     // return res.status(500).send("Error!");
 });
