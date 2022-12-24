e46b38c307421c0ea07752ba106977e6462e9afc
z20axa <alejandrospan@hotmail.com>
2022-12-24 15:47:47 -0500

    Adds initial code for DELETE notes route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,32 +4,38 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-
 router.get('/notes', (req, res) => {
     res.json(data);
 });
 
 router.post('/notes', (req, res) => {
-
     // console.log(req);
     console.log(req.body);
-    // if (typeof req.body.tittle !== 'undefined' &&
-    //     typeof req.body.text !== 'undefined'
-    // ) {
-        data.push({ ...req.body, id:uuidv4() });
-        // console.log(data);
-        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(data), (err) => {
-            // err ? console.error(err) : console.log("File Written!");
-            if (err) {
-                console.error(err);
-                res.status(500).send("Error!");
-            } else {
-                res.status(200).json(req.body);
-            }
-                
-        });
-    // }
-    // return res.status(500).send("Error!");
+    data.push({ ...req.body, id: uuidv4()});
+    // console.log(data);
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(data), (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error!");
+        } else {
+            res.status(200).json(req.body);
+        }
+    });
 });
 
-module.exports=router;
+router.delete('/notes/:id', (req, res) => {
+    // console.log('Request', req);
+    // console.log('Response', res);
+    console.log('Request Body', req.body);
+
+    // fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(data), (err) => {
+    //     if (err) {
+    //         console.error(err);
+    //         res.status(500).send("Error!");
+    //     } else {
+    //         res.status(200).json(req.body);
+    //     }
+    // });
+});
+
+module.exports = router;
